b6a89ab008227846f11318b0b69e96cfc2af87b3
Alphonse Reitz <alphonsejason18@gmail.com>
2023-07-27 03:09:32 -0500

    css background change

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 let nR = Math.floor(Math.random() * 255)
 const randBackground = {
 
@@ -5,9 +7,18 @@ const randBackground = {
     gValue: 0,
     bValue: 0,
     newRGBValue() {
-    
     this.rValue = nR;
     this.gValue = nR;
     this.bValue = nR;
-    }
+    },
+}
+
+const backGrounds = () => {
+let newBacks = document.body
+newBacks.backgroundImage = linearGradient(nR, rgb(randBackground.rValue, randBackground.gValue, randBackground.bValue));
+
+}
+
+while(1) {
+    backGrounds();
 }
