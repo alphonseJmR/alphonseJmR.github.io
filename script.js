2ba7bcdf70eb92ebb027b986a9914ae638efa924
Alphonse Reitz <alphonsejason18@gmail.com>
2023-07-27 14:02:48 -0500

    background image attempt

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
+import style from "./style_sheet_main.css";
+
 let nR = () => {
     let rand = Math.floor(Math.random() * 255)
     return rand
 }
 const randBackground = {
 
-    rValue: 0,
-    gValue: 0,
-    bValue: 0,
-    newRGBValue() {
-    this.rValue = nR();
-    this.gValue = nR();
-    this.bValue = nR();
-    },
+    rValue: nR(),
+    gValue: nR(),
+    bValue: nR(),
 }
 
 let backGrounds = () => {
 
-let newBacks = document.body
-randBackground.newRGBValue();
+let newBacks = document.querySelector("body")
 newBacks.style.backgroundImage = linearGradient(nR(), rgb(randBackground.rValue, randBackground.gValue, randBackground.bValue), rgb(randBackground.bValue, randBackground.rValue, randBackground.gValue));
-
+newBacks.style.backroundColor = 'green';
 }
 
+const newWidth = () => {
+    let goodWidth = document.getElementsByClassName(".header-container");
+    goodWidth.style.width = '70%';
+}
+
+newWidth();
 while(1) {
     backGrounds();
 }
